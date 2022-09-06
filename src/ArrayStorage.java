fa2a9495995c3346d58de81d9539a88a38c4b9ae
Smile78 <dronussr@gmail.com>
2022-09-06 22:19:28 +0300

    build v1: all tests done

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,29 +2,76 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
+
     Resume[] storage = new Resume[10000];
 
+
     void clear() {
+        for (int i = 0; i < size(); i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        storage[size()] = r; //TODO может поле size?
     }
 
     Resume get(String uuid) {
-        return null;
+        //TODO может типо mergesort?
+
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];      // вроде уникальные отсортированые ..
+//               break;
+            }
+        }
+        return null;                    // ..??
+
     }
 
     void delete(String uuid) {
+        // если неск                        - вроде уникальные
+        // если нету такого                 - ... ??
+        // size                             - пока авто пересчет
+        // если удалить из середины         - ok
+
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+//                storage[i]=null ;                     // depricated
+
+//                for (int j = i; j < size(); j++) {    // depricated
+//                    storage[j] = storage[j + 1];
+//                }
+//                break;
+
+//                if (size() - i >= 0) {
+                System.arraycopy(storage, i + 1, storage, i, size() - i);
+                break;             //чтоб удалил только один такой- даже если уникальные ) и можно свап
+//                }
+
+
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
+
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] res2 = new Resume[size()];
+        System.arraycopy(storage, 0, res2, 0, size());
+        return res2;
     }
 
     int size() {
-        return 0;
+        int cnt = 0;
+        for (Resume rsm : storage) {
+            if (rsm != null) cnt++;
+            else break;         // ?  сокращает цикл вроде ..
+        }
+        return cnt;
     }
+
+
 }
--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -2,9 +2,11 @@
  * Test for your ArrayStorage implementation
  */
 public class MainTestArrayStorage {
+
     static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) {
+
         Resume r1 = new Resume();
         r1.uuid = "uuid1";
         Resume r2 = new Resume();
